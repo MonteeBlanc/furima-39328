b1593a61b3aa55dbdb77b3a5a489470330703200
MonteeBlanc <mont_blanc925@yahoo.co.jp>
2023-06-08 21:25:29 +0900

    テストコード修正

--- spec/factories/purchase_records.rb
+++ spec/factories/purchase_records.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :purchase_record do
-    
+    association :item
+    price { Faker::Commerce.price(range: 100..10000) }
+    token { "tok_abcdefghijk00000000000000000" }
   end
 end
--- spec/models/purchase_record_spec.rb
+++ spec/models/purchase_record_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseRecord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    category = FactoryBot.create(:category, name: 'カテゴリー')
+    condition = FactoryBot.create(:condition, name: '新品')
+    shipping_day = FactoryBot.create(:shipping_day, name: '1-2日で発送')
+    shipping_fee_payer = FactoryBot.create(:shipping_fee_payer, name: '送料込み（出品者負担）')
+    shipping_area = FactoryBot.create(:shipping_area, name: '都道府県')
+    @item = FactoryBot.build(:item, condition: condition, category: category, shipping_day: shipping_day, shipping_fee_payer: shipping_fee_payer, shipping_area: shipping_area)
+    @purchase_record = FactoryBot.build(:purchase_record, item_price: @item.price, item: @item)
+  end
+
+  context '内容に問題がある場合' do
+    it 'item_priceが空では保存できないこと' do
+      @purchase_record.item_price = nil
+      expect(@purchase_record).not_to be_valid
+    end
+
+    it 'tokenが空では登録できないこと' do
+      @purchase_record.token = nil
+      expect(@purchase_record).not_to be_valid
+    end
+  end
 end
+
+
+
+
+
+
+
+
