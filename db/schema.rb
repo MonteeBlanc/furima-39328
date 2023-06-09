256fa87dc82f0874b3573fa1dc0b98383c6bab7a
MonteeBlanc <mont_blanc925@yahoo.co.jp>
2023-06-09 17:18:57 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_02_150922) do
+ActiveRecord::Schema.define(version: 2023_06_05_075240) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -87,6 +87,19 @@ ActiveRecord::Schema.define(version: 2023_06_02_150922) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "shipping_informations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.string "phone_number", null: false
+    t.integer "shipping_area_id", null: false
+    t.string "city", null: false
+    t.string "street", null: false
+    t.string "building"
+    t.bigint "purchase_record_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["purchase_record_id"], name: "index_shipping_informations_on_purchase_record_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -109,4 +122,5 @@ ActiveRecord::Schema.define(version: 2023_06_02_150922) do
   add_foreign_key "items", "users"
   add_foreign_key "purchase_records", "items"
   add_foreign_key "purchase_records", "users"
+  add_foreign_key "shipping_informations", "purchase_records"
 end
